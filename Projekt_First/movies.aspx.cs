69cc70cbc75e95ac8ce5c10931c1257f148c14a7
agdalena <chica.bizcochito@gmail.com>
2017-04-22 15:42:25 +0200

    kodadd

--- Projekt_First/movies.aspx.cs
+++ Projekt_First/movies.aspx.cs
@@ -9,6 +9,16 @@ namespace Projekt_First
 {
     public partial class movies : System.Web.UI.Page
     {
+        int wybrane2;
+        String wybrane;
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String wybrane = GridView1.SelectedDataKey.Value.ToString();
+            wybrane2 = Convert.ToInt32(GridView1.SelectedDataKey.Value);
+            XmlDataSource1.XPath = "/FilmsList/FILM[@ID=" + wybrane + "]";
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             string parameter = getParam(Const.PARAM);
